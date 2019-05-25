112e76b71794e989b77c38e6fb57f116e435be51
WyvernDie <art20022712@mail.ru>
2019-05-25 12:42:18 +0300

    Create news.php

---
+++ Parts/news.php
@@ -0,0 +1,87 @@
+<?php
+require_once '//dbConnecting.php';
+require_once '//check.php';
+require 'Header.php';
+?>
+<div class="novosti-on">
+<div class="novosti-icon">
+    <div class="slovo">
+        <li>НОВОСТИ</li>
+        <hr>
+    </div>
+</div>
+    <div class="novosti-big-block">
+        <div class="novocti-block">
+            <div class="img-novosti">
+    <img src="#">
+            </div>//
+            <hr>
+            <div class="novosti-text">
+
+            </div>//
+            <hr>
+            <div class="data">
+
+            </div>//
+        </div>
+        <div class="novocti-block"> <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        <div class="novocti-block"> <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        <div class="novocti-block">
+            <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        <div class="novocti-block">
+            <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        </div>
+    </div>
+<div>
+<?php
+require 'Footer.php';
+?>
+
