2d23fd43a1d7ab7490d107eea3127d017f3d78de
WyvernDie <art20022712@mail.ru>
2019-05-26 11:47:24 +0300

    Revert "D"
    This reverts commit 9e371f5248e230fffa8df64999f6aa8100243166.

---
+++ Parts/news.php
@@ -0,0 +1,90 @@
+<?php
+<<<<<<< HEAD
+require_once '//dbConnecting.php';
+require_once '//check.php';
+=======
+>>>>>>> f5820448b905be51e4e155f1df5a3d5c7fa89edc
+require 'Header.php';
+?>
+<div class="novosti-on">
+<div class="novosti-icon">
+    <div class="slovo">
+        <li>НОВОСТИ</li>
+        <hr>
+    </div>
+</div>
+    <div class="novosti-big-block">
+        <div class="novocti-block">
+            <div class="img-novosti">
+    <img src="#">
+            </div>//
+            <hr>
+            <div class="novosti-text">
+
+            </div>//
+            <hr>
+            <div class="data">
+
+            </div>//
+        </div>
+        <div class="novocti-block"> <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        <div class="novocti-block"> <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        <div class="novocti-block">
+            <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        <div class="novocti-block">
+            <div class="novocti-block">
+                <div class="img-novosti">
+
+                </div>//
+                <hr>
+                <div class="novosti-text">
+
+                </div>//
+                <hr>
+                <div class="data">
+
+                </div>//
+            </div>
+        </div>
+    </div>
+<div>
+<?php
+require 'Footer.php';
+?>
+
