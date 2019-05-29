a5aca1b600c72cf58306984224ed10fecfd0101f
WyvernDie <art20022712@mail.ru>
2019-05-29 15:15:47 +0300

    AboutUs
    Добавлены основные блоки

--- Parts/Header.php
+++ Parts/Header.php
@@ -24,6 +24,7 @@ while ($i < $k) {
         <link rel="stylesheet" href="../CSS/admin.css"/>
         <link rel="stylesheet" href="../CSS/sliderStyle.css">
         <link rel="stylesheet" href="../CSS/responsiveDesign.css"/>
+        <link rel="stylesheet" href="../CSS/aboutUsStyles.css"/>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script src="js/jquery-3.1.1.min.js"></script>
         <script src="../JavaScript/hearerMenu.js" type="text/javascript"></script>
