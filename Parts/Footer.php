61a08bb30c86eea0cb06f7fb901d1b676577ae09
WyvernDie <art20022712@mail.ru>
2019-06-20 22:23:55 +0300

    Footer (Content)
    I added the basic structure of the footer

--- Parts/Footer.php
+++ Parts/Footer.php
@@ -1,7 +1,42 @@
         </div>
         <footer>
-            <div class="center">
-                gadg
+            <div class="footerMid">
+
+                <div class="footerContent">
+                    <div class="footerIcon">
+                        <img src="../Image/FooterIcon/phone.png" alt="">
+                    </div>
+                    <div class="footerInfoName">
+                        Телефон
+                    </div>
+                    <div class="footerInfo">
+                        8-(977)-271-87-34
+                    </div>
+                </div>
+
+                <div class="footerContent">
+                    <div class="footerIcon">
+                        <img src="../Image/FooterIcon/email.png" alt="">
+                    </div>
+                    <div class="footerInfoName">
+                        Почта
+                    </div>
+                    <div class="footerInfo">
+                        art20022712@mail.ru
+                    </div>
+                </div>
+
+                <div class="footerContent">
+                    <div class="footerIcon">
+                        <img src="../Image/FooterIcon/vk.png" alt="">
+                    </div>
+                    <div class="footerInfoName">
+                        Вконтакте
+                    </div>
+                    <div class="footerInfo">
+                        id317218875
+                    </div>
+                </div>
             </div>
         </footer>
     </div>
