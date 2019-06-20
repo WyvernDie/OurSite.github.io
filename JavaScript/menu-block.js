a063ad3574213467045760facf8ab411bb66a7ef
Ovakimyan99 <tigr.kman@gmail.com>
2019-06-20 21:21:47 +0300

    news - over
    подкорректировал то что есть

--- JavaScript/menu-block.js
+++ JavaScript/menu-block.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     var add = document.getElementById('phone-menu');
     let count = 1;
+    var width=screen.width;
+
+
     document.getElementById('phone-menu').style.right = '0px';
 
     //  $('#elem').click(function({
@@ -11,22 +14,26 @@ $(document).ready(function() {
         if ($('#elem').css('marginRight') == '30px') {
             if (count == 1) {
                 document.getElementById('phone-menu').style.right = '168px';
+                // differe = width-127;
+                //127 - ширина блока меню
+                // document.getElementById('phone-menu').style.left =`differe`;
+                // document.getElementById('phone-menu').style.display = 'flex';
+                // document.getElementById('phone-menu').style.margineLeft =0;
                 count--;
+
             } else {
                 document.getElementById('phone-menu').style.right = '0px';
                 count++;
-            }
-            ;
+            };
         } else {
             document.getElementById('elem').onclick = function (e) {
                 if (count == 1) {
-                    document.getElementById('phone-menu').style.right = '196px';
+                    document.getElementById('phone-menu').style.right = '127px';
                     count--;
                 } else {
-                    document.getElementById('phone-menu').style.right = '70px';
+                    document.getElementById('phone-menu').style.right = '0px';
                     count++;
-                }
-                ;
+                };
             };
         }
     }
