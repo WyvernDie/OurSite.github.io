10e3ae150fc9aa6bdf301dc0c82e8c6131957155
WyvernDie <art20022712@mail.ru>
2019-06-21 00:31:36 +0300

    Footer(all)
    Make all

--- Parts/Footer.php
+++ Parts/Footer.php
@@ -2,39 +2,53 @@
         <footer>
             <div class="footerMid">
 
-                <div class="footerContent">
-                    <div class="footerIcon">
-                        <img src="../Image/FooterIcon/phone.png" alt="">
+                <div class="footerContent footerContentPhone">
+                    <div class="footerIcon footerIconPhone">
+                        <div class="footerIconShadow">
+                            <a href="#">
+                                <img src="../Image/FooterIcon/phone.png" alt="">
+                            </a>
+                        </div>
                     </div>
                     <div class="footerInfoName">
-                        Телефон
+                        <a href="tel:+79772718734">ТЕЛЕФОН</a>
                     </div>
                     <div class="footerInfo">
-                        8-(977)-271-87-34
+                        <a href="tel:+79772718734">8-(977)-271-87-34</a>
                     </div>
                 </div>
 
-                <div class="footerContent">
-                    <div class="footerIcon">
-                        <img src="../Image/FooterIcon/email.png" alt="">
+                <div class="footerContent footerContentEmail">
+                    <div class="footerIcon footerIconEmail">
+                        <div class="footerIconShadow">
+                            <a href="#">
+                                <img src="../Image/FooterIcon/email.png" alt="">
+                            </a>
+                        </div>
                     </div>
                     <div class="footerInfoName">
-                        Почта
+                        <a href="mailto:art20022712@mail.ru?subject=Вопрос по игровому клубу Shelter">ПОЧТА</a>
                     </div>
                     <div class="footerInfo">
-                        art20022712@mail.ru
+                        <a href="mailto:art20022712@mail.ru?subject=Вопрос по игровому клубу Shelter">art20022712@mail.ru</a>
                     </div>
                 </div>
 
-                <div class="footerContent">
-                    <div class="footerIcon">
-                        <img src="../Image/FooterIcon/vk.png" alt="">
+                <div class="footerContent footerContentVk">
+                    <div class="footerIcon footerIconVk">
+                        <div class="footerIconShadow ">
+                            <a href="#">
+                                <img src="../Image/FooterIcon/vk.png" alt="">
+                            </a>
+                        </div>
                     </div>
                     <div class="footerInfoName">
-                        Вконтакте
+                        <a href="https://vk.com/id317218875">ВКОНТАКТЕ</a>
+
                     </div>
                     <div class="footerInfo">
-                        id317218875
+                        <a href="https://vk.com/id317218875">Напишите нам</a>
+
                     </div>
                 </div>
             </div>
--- Parts/Header.php
+++ Parts/Header.php
@@ -16,6 +16,7 @@ while ($i < $k) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width= device-width, initial-scale=1.0"/>
+    <meta name="format-detection" content="telephone=no">
     <title>Сайт</title>
     <link rel="stylesheet" href="../CSS/Header_Footer_Styles.css" />
     <link rel="stylesheet" href="../CSS/Global.css"/>
