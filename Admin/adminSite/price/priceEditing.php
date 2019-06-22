96bad8f6254b3321a92481380a5dbb0b009d0fe4
WyvernDie <art20022712@mail.ru>
2019-06-22 12:12:22 +0300

    adsdasd

---
+++ Admin/adminSite/price/priceEditing.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../../../Actions/dbConnecting.php';
+require_once '../../../Actions/check.php';
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="viewport" content="width= device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="../../../CSS/game.css"/>
+        <link rel="stylesheet" href="../../../CSS/admin.css"/>
+        <link rel="stylesheet" href="../../../CSS/sliderStyle.css">
+    </head>
+    <body>
+    <a href="../../../index.php" class="main"><h1>На Главную</h1></a>
+    <table>
+
+    </table>
+
+    </body>
+</html>
