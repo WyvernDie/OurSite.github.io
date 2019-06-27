301a47550eade86da297bdea7eb1c9590eff3271
Ovakimyan99 <tigr.kman@gmail.com>
2019-06-27 13:30:25 +0300

    o nas parth 1

--- aboutUs.php
+++ aboutUs.php
@@ -2,57 +2,63 @@
 require 'Parts/Header.php';
 ?>
 <div class="wrappOnas">
-    <h1></h1>
+    <h1>Loewm ipsum</h1>
     <div class="infoBlocks">
         <div class="infoBlock" id="infoBlock1">
                     <div class="infoBlockText">
+                        <div class="infoBlockHelp">
                         <hr>
-                        <h2></h2>
-                        <p></p>
-                        <hr>
+                        <h2>Lorem ipsum</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt dslkfsdf fk;sdkf;lskf;ldk sdkjnma,;cs;ldc jsdklfmsdlkfmksfsd zxcvnm qwrtsyui wqwtshjk</p>
+                        </div>
+                        <hr id="hrBottom">
                     </div>
                 <div class="infoBlockImg">
                     <hr>
-                    <img src="" alt="" id="" class="">
+                    <img src="../Image/news/1.jpg" alt="тутуттут" id="" class="">
                     <hr>
                 </div>
         </div>
-    </div>
 
 
-    <div class="infoBlocks">
+
+
         <div class="infoBlock" id="infoBlock2">
             <div class="infoBlockText">
-                <hr>
-                <h2></h2>
-                <p></p>
-                <hr>
+                <div class="infoBlockHelp">
+                    <hr>
+                    <h2>Lorem ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt dslkfsdf fk;sdkf;lskf;ldk sdkjnma,;cs;ldc jsdklfmsdlkfmksfsd zxcvnm qwrtsyui wqwtshjk</p>
+                </div>
+                <hr id="hrBottom">
             </div>
             <div class="infoBlockImg">
                 <hr>
-                <img src="" alt="" id="" class="">
+                <img src="../Image/news/1.jpg" alt="тутуттут" id="" class="">
                 <hr>
             </div>
         </div>
-    </div>
 
 
-    <div class="infoBlocks">
+
+
         <div class="infoBlock" id="infoBlock3">
             <div class="infoBlockText">
-                <hr>
-                <h2></h2>
-                <p></p>
-                <hr>
+                <div class="infoBlockHelp">
+                    <hr>
+                    <h2>Lorem ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt dslkfsdf fk;sdkf;lskf;ldk sdkjnma,;cs;ldc jsdklfmsdlkfmksfsd zxcvnm qwrtsyui wqwtshjk</p>
+                </div>
+                <hr id="hrBottom">
             </div>
             <div class="infoBlockImg">
                 <hr>
-                <img src="" alt="" id="" class="">
+                <img src="../Image/news/1.jpg" alt="тутуттут" id="" class="">
                 <hr>
             </div>
         </div>
     </div>
-</div>
+
 <div class="wrappIron">
     <h2></h2>
     <div class="ironElements">
