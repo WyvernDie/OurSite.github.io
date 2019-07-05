698f55f4c296f4ccf0288f205b7fb647a8c257f6
Ovakimyan99 <tigr.kman@gmail.com>
2019-07-05 11:23:44 +0300

    предконец

--- aboutUs.php
+++ aboutUs.php
@@ -22,7 +22,6 @@ require 'Parts/Header.php';
 
 
 
-
         <div class="infoBlock" id="infoBlock2">
             <div class="infoBlockText">
                 <div class="infoBlockHelp">
@@ -63,50 +62,59 @@ require 'Parts/Header.php';
     <h2>ЖЕЛЕЗО</h2>
     <div class="ironElements">
             <div class="ironImg owl-carousel owl-theme" id="ironCarousel">
-                <div class="ironImgHelp" id="ironImgHelp1">
+                    <div class="ironImgHelp" id="ironImgHelp1">
                 <img src="../Image/Games/GTA5.jpg" id="ironImg1" alt="">
-<!--                <img src="../Image/iSeeYou1.jpg" id="ironOpen" alt="">-->
-<!--                <img src="../Image/iSeeYou2.jpg" id="ironClose" alt="">-->
-<!--                            <p id="imgText1">1tatatattatatatttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt</p>-->
-                </div>
+                <img src="../Image/iSeeYou1.jpg"   class="transform"  id="ironOpen" alt="">
+                <img src="../Image/iSeeYou2.jpg"   class="transform"  id="ironClose" alt="">
+                    </div>
                         <div class="ironImgHelp" id="ironImgHelp2">
-                <img src="../Image/Games/GTA5.jpg" id="ironImg2" alt="">
-<!--                <img src="../Image/iSeeYou1.jpg" id="ironOpen" alt="">-->
-<!--                <img src="</../Image/iSeeYou2.jpg" id="ironClose" alt="">-->
-<!--                            <p id="imgText2">2tatatattatatatttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt</p>-->
+                <img src="../Image/Games/c.jpg" id="ironImg2" alt="">
+                <img src="../Image/iSeeYou1.jpg"  class="transform"  id="ironOpen" alt="">
+                <img src="../Image/iSeeYou2.jpg"  class="transform"  id="ironClose" alt="">
                         </div>
                 <div class="ironImgHelp" id="ironImgHelp3">
                 <img src="../Image/Games/GTA5.jpg" id="ironImg3" alt="">
-<!--                <img src="../Image/iSeeYou1.jpg" id="ironOpen" alt="">-->
-<!--                <img src="../Image/iSeeYou2.jpg" id="ironClose" alt="">-->
-<!--                        <p id="imgText3">3tatatattatatatttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt</p>-->
+                <img src="../Image/iSeeYou1.jpg"  class="transform" id="ironOpen" alt="">
+                <img src="../Image/iSeeYou2.jpg"  class="transform" id="ironClose" alt="">
                     </div>
                         <div class="ironImgHelp" id="ironImgHelp4">
-                <img src="../Image/Games/GTA5.jpg" id="ironImg4" alt="">
-<!--                <img src="../Image/iSeeYou1.jpg" id="ironOpen" alt="">-->
-<!--                <img src="../Image/iSeeYou2.jpg" id="ironClose" alt="">-->
-<!--                            <p id="imgText4">4tatatattatatatttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt</p>-->
+                <img src="../Image/Games/c.jpg" id="ironImg4" alt="">
+                <img src="../Image/iSeeYou1.jpg"  class="transform"  id="ironOpen" alt="">
+                <img src="../Image/iSeeYou2.jpg"  class="transform"  id="ironClose" alt="">
                         </div>
                             <div class="ironImgHelp" id="ironImgHelp5">
                 <img src="../Image/Games/GTA5.jpg" id="ironImg5" alt="">
-<!--                <img src="../Image/iSeeYou1.jpg" id="ironOpen" alt="">-->
-<!--                <img src="../Image/iSeeYou2.jpg" id="ironClose" alt="">-->
-<!--                                <p id="imgText5">5tatatattatatatttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt</p>-->
+                <img src="../Image/iSeeYou1.jpg"  class="transform" id="ironOpen" alt="">
+                <img src="../Image/iSeeYou2.jpg"  class="transform" id="ironClose" alt="">
                             </div>
             </div>
+
+        <div class="ironInfo owl-carousel owl-theme" id="ironInfoD">
+            <p id="ironInfo1">1ldssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+            <p id="ironInfo2">21ldssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+            <p id="ironInfo3">31ldssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+            <p id="ironInfo4">41ldssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+            <p id="ironInfo5">51ldssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+        </div>
+
                    <hr id="imgTextHr">
             <div class="imgNav">
-<!--                <div class="prev"><</div>-->
-<!--                <div class="next">></div>-->
-                <div class="imgNavParths">
-                    <h2 id="imgNavElH21">Видеокарта</h2>
-                    <h2 id="imgNavElH22">Процессор</h2>
-                    <h2 id="imgNavElH23">Маткринская плата</h2>
-                    <h2 id="imgNavElH24">Что то еще</h2>
-                    <h2 id="imgNavElH25">тут тоже что то</h2>
-                </div>
-            </div>
-    </div>
+
+                <img src="../Image/navLeftImg.jpg" id="slideLeft" class="slide-controls__left">
+
+                    <div class="slide-controls-word">
+
+                        <div id="slide-controlText" class="slide-control owl-carousel owl-theme" style="margin-top: 0">
+                                <div><p class="slide-controlElem">видюха1</p></div>
+                                <div><p class="slide-controlElem">видюха2</p></div>
+                                <div><p class="slide-controlElem">видюха3</p></div>
+                                <div><p class="slide-controlElem">видюха4</p></div>
+                                <div><p class="slide-controlElem">видюха5</p></div>
+                        </div>
+
+                    </div>
+
+                <img src="../Image/navRightImg.jpg" id="slideRight" class="slide-controls__right" alt="вправо">
 </div>
 
 
@@ -121,47 +129,70 @@ require 'Parts/Header.php';
         $(document).ready(function () {
             $("#ironCarousel").owlCarousel({
                 items: 3,
-                dots: true,
+                touchDrag: false,
+                mouseDrag:false,
+                loop: true,
+                dots: false,
                 responsive: {
                     0: {
+                        nav: false,
                         items: 1,
-                        dots: true,
+                        dots: false,
                         margin: 10,
                     },
                     240: {
+                        nav: false,
                         items: 1,
                         margin: 10,
-                        dots: true
+                        dots: false
                     },
                     320: {
+                        nav: false,
                         items: 1,
                         margin: 10,
-                        dots: true
+                        dots: false
                     },
                     480: {
-                        dots: true,
+                        dots: false,
                         margin: 10,
                         items: 2,
-                        nav: false
+                        nav: false,
                     },
                     640: {
-                        dots: true,
+                        dots: false,
+                        nav: false,
                         items: 3,
-                        nav: false
                     },
                     876: {
-                        dots: true,
-                        items: 4,
-                        nav: false
+                        dots: false,
+                        nav: false,
+                        items: 3,
+                        margin: 10
                     },
                     1300: {
                         items: 3,
-                        dots: true,
-                        nav: true,
+                        dots: false,
+                        nav: false,
                         margin: 10
                     }
                 }
             });
+                $('#slide-controlText').owlCarousel({
+                margin: 10,
+                    touchDrag: false,
+                    mouseDrag:false,
+                    loop: true,
+                    items: 1,
+                dots: false
+            });
+                $('#ironInfoD').owlCarousel({
+                    margin: 10,
+                    touchDrag: false,
+                    mouseDrag:false,
+                    loop: true,
+                    items: 1,
+                    dots: false
+                });
         });
     </script>
 <?php
